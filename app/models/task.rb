be5ffaa68758d77ad1b1bbe6739786d63c68911f
hanzawa825 <157540704+hanzawa825@users.noreply.github.com>
2024-12-13 01:34:18 +0900

    add/スケジュールの作成

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  validates :title, presence: { message: "を入力してください" }
+  validates :start_date, presence: { message: "を入力してください" }
+  validates :end_date, presence: { message: "を入力してください" }
 end
